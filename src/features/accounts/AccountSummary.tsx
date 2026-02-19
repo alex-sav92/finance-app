30417257fa0d5181dd36ee7f1c971564b3cf83ee
alex-sav92 <alexandrasavescu92@gmail.com>
2026-02-19 22:39:21 +0200

    Calculate balance

--- src/features/accounts/AccountSummary.tsx
+++ src/features/accounts/AccountSummary.tsx
@@ -1,3 +1,4 @@
+import { useTransactions } from '../transactions/useTransactions'
 import { useAccountSummary } from './useAccountSummary'
 
 type Props = {
@@ -5,8 +6,13 @@ type Props = {
 }
 
 export function AccountSummary({ accountId }: Props) {
-  const { summary, balance, loading } = useAccountSummary(accountId)
+  const { summary, loading } = useAccountSummary(accountId)
+  const { transactions } = useTransactions(accountId) // extract the array
 
+  const balance = transactions.reduce(
+    (sum, tx) => sum + Number(tx.amount), // make sure tx.amount is a number
+    0
+  )
   if (!accountId) return null
   if (loading) return <p>Loading summary...</p>
 
@@ -14,11 +20,10 @@ export function AccountSummary({ accountId }: Props) {
     <div>
       <h3>Account Summary</h3>
       <div className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white p-6 rounded-xl shadow">
-        <div className="text-sm opacity-80">Current Balance</div>
         <div className="text-3xl font-bold mt-2">
-          € 2,430
+          {balance.toLocaleString('en-US', { style: 'currency', currency: 'RON' })}
         </div>
-</div>
+      </div>
 
       {summary.length ? (
         <>
