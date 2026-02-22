bf9b58212141149445d1d83d4508c3ee9e15846f
alex-sav92 <alexandrasavescu92@gmail.com>
2026-02-23 00:31:34 +0200

    Reorganize main files and App file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { TestService } from './core/test.service';
-import { SessionService } from './core/session.service';
-import { LoginComponent } from "./features/auth/login/login.component";
-import { RouterOutlet } from "@angular/router";
+// src/app/app.component.ts
+// src/app/features/transactions/add-transaction.component.ts
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SessionService } from './core/session.service';
+import { LoginComponent } from './features/auth/login/login.component';
+import { AddTransactionComponent } from './features/transactions/add-transaction/add-transaction.component';
+import { TransactionsListComponent } from './features/transactions/transaction-list/transaction-list.component';
+import { AccountsListComponent } from './features/accounts/accounts-list/accounts-list.component';
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    LoginComponent,
+    AccountsListComponent,
+    TransactionsListComponent,
+    AddTransactionComponent,
+  ],
   template: `
     <div *ngIf="user; else loginTemplate">
       <header>
@@ -13,30 +25,24 @@ import { CommonModule } from '@angular/common';
         <button (click)="logout()">Logout</button>
       </header>
 
-      <router-outlet></router-outlet>
+      <app-accounts-list></app-accounts-list>
+      <app-transactions-list></app-transactions-list>
+      <app-add-transaction></app-add-transaction>
     </div>
 
     <ng-template #loginTemplate>
       <app-login></app-login>
     </ng-template>
   `,
-  imports: [CommonModule, LoginComponent, RouterOutlet],
 })
-export class AppComponent implements OnInit {
-  user: any; 
-  testService: any;
+export class AppComponent {
+  user: any;
+
   constructor(private session: SessionService) {
     this.session.user$.subscribe(u => (this.user = u));
   }
 
   logout() {
     this.session.logout();
-  }
-
-  ngOnInit() {}
-
-  async test() {
-    const result = await this.testService.testConnection();
-    console.log('Result:', result);
   }
 }
