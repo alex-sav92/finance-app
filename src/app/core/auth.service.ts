fa2175df05aedcf0dcc8f2d94641ed891b65f8f5
alex-sav92 <alexandrasavescu92@gmail.com>
2026-02-24 23:32:21 +0200

    auth - get current user and logout methods

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -6,9 +6,18 @@ import { supabase } from './supabase.client';
   providedIn: 'root',
 })
 export class AuthService {
+ 
   async login(email: string) {
     const { data, error } = await supabase.auth.signInWithOtp({ email });
     if (error) throw error;
     return data;
+  }
+   async getUser() {
+    const { data } = await supabase.auth.getUser();
+    return data.user;
+  }
+
+  async logout() {
+    await supabase.auth.signOut();
   }
 }
