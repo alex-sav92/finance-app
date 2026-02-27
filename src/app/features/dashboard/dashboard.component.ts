1518d3a9daba69e81f28ad9bc4c0fdf89ae18923
alex-sav92 <alexandrasavescu92@gmail.com>
2026-02-27 19:46:59 +0200

    Make category chart Pie chart

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { TransactionService } from '../../services/transaction.service';
 import { AccountService } from '../../services/account.service';
 import { CommonModule } from '@angular/common';
 import { NgChartsModule } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -12,15 +13,18 @@ import { NgChartsModule } from 'ng2-charts';
 })
 export class DashboardComponent implements OnInit {
 
+  public pieChartType: ChartType = 'pie';
+
+  public pieChartData: any; 
   accounts: any[] = [];
   transactions: any[] = [];
   monthlyLabels: string[] = [];
-monthlyIncome: number[] = [];
-monthlyExpenses: number[] = [];
+  monthlyIncome: number[] = [];
+  monthlyExpenses: number[] = [];
 
-categoryLabels: string[] = [];
-categoryColors: string[] = [];
-categoryData: number[] = [];
+  categoryLabels: string[] = [];
+  categoryColors: string[] = [];
+  categoryData: number[] = [];
 
   totalBalance = 0;
   income = 0;
@@ -45,69 +49,78 @@ categoryData: number[] = [];
 
   calculateStats() {
 
-  this.totalBalance = this.accounts.reduce(
-    (sum, acc) => sum + Number(acc.balance || 0),
-    0
-  );
+    this.totalBalance = this.accounts.reduce(
+      (sum, acc) => sum + Number(acc.balance || 0),
+      0
+    );
 
-  this.income = 0;
-  this.expenses = 0;
+    this.income = 0;
+    this.expenses = 0;
 
-  const monthlyMap: any = {};
-  const categoryMap: any = {};
+    const monthlyMap: any = {};
+    const categoryMap: any = {};
 
-  for (const t of this.transactions) {
+    for (const t of this.transactions) {
 
-    const amount = Number(t.amount);
-    const date = new Date(t.date);
-    const month = date.toLocaleString('default', { month: 'short' });
+      const amount = Number(t.amount);
+      const date = new Date(t.date);
+      const month = date.toLocaleString('default', { month: 'short' });
 
-    if (!monthlyMap[month]) {
-      monthlyMap[month] = { income: 0, expense: 0 };
-    }
-
-    if (t.type === 'income') {
-      this.income += amount;
-      monthlyMap[month].income += amount;
-    } else {
-      this.expenses += amount;
-      monthlyMap[month].expense += amount;
-
-      const category = t.categories?.name ?? 'Other';
-
-      if (!categoryMap[category]) {
-        categoryMap[category] = 0;
+      if (!monthlyMap[month]) {
+        monthlyMap[month] = { income: 0, expense: 0 };
       }
 
-      categoryMap[category] += amount;
+      if (t.type === 'income') {
+        this.income += amount;
+        monthlyMap[month].income += amount;
+      } else {
+        this.expenses += amount;
+        monthlyMap[month].expense += amount;
+
+        const category = t.categories?.name ?? 'Other';
+
+        if (!categoryMap[category]) {
+          categoryMap[category] = 0;
+        }
+
+        categoryMap[category] += amount;
+      }
     }
-  }
 
-  this.monthlyLabels = Object.keys(monthlyMap);
+    this.monthlyLabels = Object.keys(monthlyMap);
 
-  this.monthlyIncome = this.monthlyLabels.map(
-    m => monthlyMap[m].income
-  );
+    this.monthlyIncome = this.monthlyLabels.map(
+      m => monthlyMap[m].income
+    );
 
-  this.monthlyExpenses = this.monthlyLabels.map(
-    m => monthlyMap[m].expense
-  );
+    this.monthlyExpenses = this.monthlyLabels.map(
+      m => monthlyMap[m].expense
+    );
 
-  this.categoryLabels = Object.keys(categoryMap);
-  this.categoryData = Object.values(categoryMap);
-  
-  this.categoryColors = [
-  '#ef4444',
-  '#f97316', 
-  '#eab308', 
-  '#22c55e',
-  '#06b6d4',
-  '#3b82f6',
-  '#8b5cf6', 
-  '#ec4899', 
-  '#14b8a6',
-  '#64748b'  
-];
+    this.categoryLabels = Object.keys(categoryMap);
+    this.categoryData = Object.values(categoryMap);
+    
+    this.categoryColors = [
+      '#ef4444',
+      '#f97316', 
+      '#eab308', 
+      '#22c55e',
+      '#06b6d4',
+      '#3b82f6',
+      '#8b5cf6', 
+      '#ec4899', 
+      '#14b8a6',
+      '#64748b'  
+    ];
+
+  this.pieChartData = {
+    labels: this.categoryLabels,
+    datasets: [
+      {
+        data: this.categoryData,
+        backgroundColor: this.categoryColors
+      }
+    ]
+  };
 }
-
 }
